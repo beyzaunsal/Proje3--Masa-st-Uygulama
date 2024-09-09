d2db841070f8e4be198e1ac84e110c1b9c177ff9
beyzaunsal <unsalbeyza678@gmail.com>
2024-09-09 19:24:54 +0300

    güncel

---
+++ Notlar/vbilimi_yzeka.py
@@ -0,0 +1,88 @@
+#pictoblox
+# import numpy
+# print(numpy.__version__)
+
+# import numpy
+# arr = numpy.array([1, 2, 3, 4, 5]) #numpy burda sınıf
+# print(arr)
+# print(type(arr))
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+# print(type(arr))
+
+###veri türleri--------------------
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+# print(type(arr))
+
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# xxx = np.array([1, 2, 3, 4, 5],np.int8)
+# print(arr)
+# print(type(arr))
+# print(type(xxx))
+# print(arr.dtype)
+# print(xxx.dtype)
+
+
+#düzenli olma---------
+
+# import numpy as np
+# arr= np.array([[1,2,5],[7,8,9]]) #eğer np.array([[1,2],[7,8,9]]) olsaydı hata verirdi.
+# arr1 = np.arange(5,15,2)
+# print(arr1)
+
+#zeros ve ones--------------
+# import numpy as np
+# arr1 = np.zeros((3,4))
+# print(arr1)
+
+# arr2 = np.ones((3,5,2))
+# print(arr2)
+
+# arr3 = np.ones((3,5))*4
+# print(arr3)
+
+
+# arange ile aralık------------------
+# import numpy as np
+
+# arr1 = np.arange(10)
+# print(arr1)
+
+# arr2 = np.arange(7,30,5)
+# print(arr2)
+
+# Birim matris------------------
+# import numpy as np
+
+# arr1 = np.eye(5)
+# print(arr1)
+
+# bir dizinin tersi
+# import numpy as np
+
+# arr1 = np.arange(1,11)
+# print("Dizi :\n",arr1)
+# arr1 = arr1.reshape(5,2) #diziyi yeniden şelille
+# print("Dizi :\n",arr1)
+
+
+# tersi = arr1[::-1]          #diziyi tersine  çevirdi
+# print("Tersi:\n",tersi) 
+
+# numpy satırlar ve sütünlar---------------------------
+import numpy as np
+
+arr1 = np.arange(1,21).reshape(5,4)
+print (arr1)
+
+# print("\nBirinci satır => arr1[0]   : ",arr1[0])
+# print("\nİkinci satır  => arr1[1]   : ",arr1[1])
+# print("\nİlk iki satır => arr1[0:2] : \n",arr1[0:2])
+# print("\nBirinci sütun => arr1[:,0] : ",arr1[:,0])
+# print("\nİkinci sütun  => arr1[:,1] : ",arr1[:,1]) 
+print("\nEleman        => arr1[2,1] : ",arr1[2,1])  
--- projee.py
+++ projee.py
@@ -4,133 +4,274 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtWidgets import QWidget
 import Rehber
 
-class AnaPencere(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def sifreOlustur():
+    kullananıAdi ="admin"
+    sifre="1234"
+    dosya = open("rhbpwd.txt","w")
+    dosya.write(f"{kullananıAdi} ==>> {sifre}")
+    dosya.close()
 
-        self.setWindowTitle("Login Ekranı")
-        self.arayuz()
+class AnaEkran(QMainWindow):
+    def __init__(self,XX ="Başlıksız"):
+        super(). __init__()
+        self.setWindowTitle(xx)
 
-    def arayuz(self):
-        ana_bilesenler = QWidget()
-        layout = QVBoxLayout()
+        icerik = QVBoxLayout()
+        self.dugme1 =QPushButton("Ekle")
+        icerik.addWidget(self.dugme1)
+        self.dugme2 =QPushButton("Listele")
+        icerik.addWidget(self.dugme2)
+        self.dugme3 =QPushButton("Ara")
+        icerik.addWidget(self.dugme3)
+        self.dugme4 =QPushButton("Sil")
+        icerik.addWidget(self.dugme4)
+        self.dugme5 =QPushButton("Düzelt")
+       
+        
+        self.dugme1.clicked.connect(self.ekle)
+        self.dugme2.clicked.connect(self.listele)
+        self.dugme3.clicked.connect(self.ara)
+        self.dugme4.clicked.connect(self.sil)
+        self.dugme5.clicked.connect(self.duzelt)
 
-        label_username = QLabel("Kullanıcı Adı:")
-        self.username_input = QLineEdit()
-        layout.addWidget(label_username)
-        layout.addWidget(self.username_input)
+        araclar =QWidget()
+        araclar.setLayout(icerik)
+        self.setCentralWidget(araclar)
 
-        label_password = QLabel("Şifre:")
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-        layout.addWidget(label_password)
-        layout.addWidget(self.password_input)
+    def ekle(self):
+            self.close()
+            self.ekleme = EklemeEkrani("Kayit Ekle")
+            self.ekleme.show()
 
-        login_button = QPushButton("Giriş Yap")
-        login_button.clicked.connect(self.kontrolEt)
-        layout.addWidget(login_button)
+    def listele(self):
+            self.close()
+            self.listeleme = ListelemeEkrani("Kayit Listele")
+            self.listeleme.show
+    
+    def ara(self):
+            self.close()
+            self.arama = AramaEkrani("Kayit Ara")
+            self.arama.show
 
-        ana_bilesenler.setLayout(layout)
-        self.setCentralWidget(ana_bilesenler)
+    def sil(self):
+            self.close()
+            self.silme = SilmeEkrani("Kayit Sil")
+            self.silme.show
+    def duzelt(self):
+            self.close()
+            self.duzeltme =DuzeltmeEkrani("Kayit Duzelt")
+            self.duzeltme.show()
+
+
+class LoginPencere(QMainWindow):
+    def __init__(self,XX ="Başlıksız"):
+        super(). __init__()
+        self.setWindowTitle(xx)
+    
+        icerik =QVBoxLayout()
+        icerik.addWidget(QLabel("Kullanıcı Adı:"))
+        self.edit1  =QLineEdit()
+        icerik.addWidget(self.edit1)
+        icerik.addWidget(QLabel("Şifre:"))
+        self.edit2 =QLineEdit()
+        self.edit2.setEchoMode(QLineEdit.EchoMode.Password)
+        icerik.addWidget(self.edit2)
+        self.dugme1 = QPushButton("Giriş Yap")
+        içerik.addWidget(self.dugme1)
+
+        self.dugme1.clicked.connect(self.kontrolEt)
+ 
+        araclar = QWidget()
+        araclar.setLayout(icerik)
+        self.setCentralWidget(araclar)
+
 
     def kontrolEt(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
-
+        print("----------------")
+        username = self.edit1.text()
+        password = self.edit2.text()
         print(f"Birinci Kutuya {username}, ikinci kutuya {password} girdiniz.")
+ 
+        dosya =open("rehbergirilen.txt","w")
+        dosya.write( f"{username}==>> {password}")
+        dosya.close()
 
-        if username == "p" and password == "p":
-            self.rehberiAc()
-
+        if username=="admin" and password == "1234" :
+            print("Giriş Başarılı")
+            self.close()
+            self.ap = AnaPencere()
+            self.ap.show()
         else:
-            QMessageBox.warning(self, "Hata", "Geçersiz kullanıcı adı veya şifre!")
+            print("Hatalı Giriş")
+            mesaj = QMessageBox(self)
+            mesaj.setWindowTitle("Bilgilendirme!")
+            mesaj.setText("İzin yok")
+            mesaj.exec()
 
-    def rehberiAc(self):
-        QMessageBox.information(self, "Başarılı", "Giriş başarılı!\ Programa giriş yaptınız.")
-        self.close()  # Login penceresini kapat
-        self.rehber_window = RehberPencere()
-        self.rehber_window.show()
-    
-class RehberPencere(QMainWindow):
-    def __init__(self):
+class EklemeEkrani(QMainWindow):
+    def __init__(self,xx="Başlıksız"):
         super().__init__()
+        self.setWindowTitle(xx)
 
-        self.setWindowTitle("Rehber")
-        self.arayuz()
+        icerik = QVBoxLayout()
+        icerik.addWidget(QLabel("Ad:"))
+        self.edit1 =QLineEdit("")
+        icerik.addWidget(self.edit1)
 
-    def arayuz(self):
-        ana_bilesenler = QWidget()
-        layout = QVBoxLayout()
+        icerik.addWidget(QLabel("Soyad:"))
+        self.edit2 =QLineEdit("")
+        icerik.addWidget(self.edit2)
 
-        eklenecek_isim=QLabel("Ad:")
-        self.isimEdit = QLineEdit()
-        layout.addWidget(eklenecek_isim)
-        layout.addWidget(self.isimEdit)
+        icerik.addWidget(QLabel("Telefon Numarası:"))
+        self.edit3 =QLineEdit("")
+        icerik.addWidget(self.edit3)
 
-        eklenecek_soyad =QLabel("Soyadı:")
-        self.soyadedit=QLineEdit()
-        layout.addWidget(eklenecek_soyad)
-        layout.addWidget(self.soyadedit)
+        self.dugme1 = QPushButton("Kaydet")
+        icerik.addWidget(self.dugme1)
+        
+        self.dugme1.clicked.connect(self.kaydet)
 
-        eklenecek_tel=QLabel("Telefon:")
-        self.teledit=QLineEdit()
-        layout.addWidget(eklenecek_tel)
-        layout.addWidget(self.teledit)
+        araclar = QWidget()
+        araclar.setLayout(icerik)
+        self.setCentralWidget(araclar)
 
-        self.save_button = QPushButton("Kaydet")
-        self.save_button.clicked.connect(self.listeAc)
-        layout.addWidget(self.save_button)
+    def kaydet(self):
+        t1 = self.edit1.text()
+        t2 = self.edit2.text()
+        t3= self. edit3.t
+        print("Edit 1 içeriği:", t1)
+        print("Edit 2 içeriği:", t2)
+        print("Edit 3 içeriği:", t3)
 
-        ana_bilesenler.setLayout(layout)
-        self.setCentralWidget(ana_bilesenler)
+        import sqlite3
+        veritabani1 = sqlite3.connect('rehber3.db')
+        svt = veritabani1.cursor()
+        svt.execute("CREATE TABLE IF NOT EXISTS isimler(id INTEGER PRIMARY KEY AUTOINCREMENT,ad,soyad,numara)")
+        svt.execute(f"INSERT INTO isimler(ad,soyad,numara) VALUES ('{t1}','{t2}','{t3}')")
+        veritabani1.commit()
+        veritabani1.close()
 
-    def listeAc(self):
-        Ad = self.isimEdit.text()
-        Soyadı = self.soyadedit.text()
-        Telefon = self.teledit.text()
+        self.close() 
+        self.ae = anaEkran() 
+        self.ae.show()
 
-        print(f"ADI :{Ad}, SOYADI : {Soyadı}, TEL :{Telefon} girdiniz.")
 
-        if Ad== "" and Soyadı == "" and Telefon =="":
-            self.listeAc()
+        print("Kayıt başarıyla yapıldı!")
+
+class ListelemeEkrani(QMainWindow):
+    def __init__(self,xx="Başlıksız"):
+        super().__init__()
+        self.setWindowTitle(xx)
+     
+        import sqlite3
+        veritabani1 = sqlite3.connet("rehber")
+        secilenvt = veritabani1.execute(f"SELECT * FROM isimler") 
+        
+        icerik =QGridLayout()
+        x=1
+        icerik.addWidget(QLabel("Adı"),0,1)
+        icerik.addWidget(QLabel("Soyadı"),0,2)
+        icerik.addWidget(QLabel("Telefon Numarası"),0,3)
+        for a in liste :
+            print (a[1],a[2],a[3])
+            icerik.addWidget(QLabel(a[1]),x,1) # gridLayout taki x.satır ve 1.sütuna QLable yerleştir.
+            icerik.addWidget(QLabel(a[2]),x,2)
+            icerik.addWidget(QLabel(a[3]),x,3)
+            x+=1
+        
+    araclar = QWidget() # Pencere widgeti oluştur.
+    araclar.setLayout(icerik) # Pencere widgeti için layout ata
+    self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
+    veritabani1.close()
+
+#     def rehberiAc(self):
+#         QMessageBox.information(self, "Başarılı", "Giriş başarılı!\ Programa giriş yaptınız.")
+#         self.close()  # Login penceresini kapat
+#         self.rehber_window = RehberPencere()
+#         self.rehber_window.show()
     
-        else:
-            QMessageBox.warning(self, "Hata", "Kişi Kaydedilemedi!")
+# class RehberPencere(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
 
-    def listeAc(self):
-        QMessageBox.information(self, "Başarılı", "Kişi Kaydedildi.")
-        self.close()  
+#         self.setWindowTitle("Rehber")
+#         self.arayuz()
+
+#     def arayuz(self):
+#         ana_bilesenler = QWidget()
+#         layout = QVBoxLayout()
+
+#         eklenecek_isim=QLabel("Ad:")
+#         self.isimEdit = QLineEdit()
+#         layout.addWidget(eklenecek_isim)
+#         layout.addWidget(self.isimEdit)
+
+#         eklenecek_soyad =QLabel("Soyadı:")
+#         self.soyadedit=QLineEdit()
+#         layout.addWidget(eklenecek_soyad)
+#         layout.addWidget(self.soyadedit)
+
+#         eklenecek_tel=QLabel("Telefon:")
+#         self.teledit=QLineEdit()
+#         layout.addWidget(eklenecek_tel)
+#         layout.addWidget(self.teledit)
+
+#         self.save_button = QPushButton("Kaydet")
+#         self.save_button.clicked.connect(self.listeAc)
+#         layout.addWidget(self.save_button)
+
+#         ana_bilesenler.setLayout(layout)
+#         self.setCentralWidget(ana_bilesenler)
+
+#     def listeAc(self):
+#         Ad = self.isimEdit.text()
+#         Soyadı = self.soyadedit.text()
+#         Telefon = self.teledit.text()
+
+#         print(f"ADI :{Ad}, SOYADI : {Soyadı}, TEL :{Telefon} girdiniz.")
+
+#         if Ad== "" and Soyadı == "" and Telefon =="":
+#             self.listeAc()
+    
+#         else:
+#             QMessageBox.warning(self, "Hata", "Kişi Kaydedilemedi!")
+
+#     def listeAc(self):
+#         QMessageBox.information(self, "Başarılı", "Kişi Kaydedildi.")
+#         self.close()  
 
 
-class KayitEklemeEkrani(QMainWindow):
-    def __init__(self):
-        super.__init__(self)
-        self.setWindowTitle("Rehberim")
+# class KayitEklemeEkrani(QMainWindow):
+#     def __init__(self):
+#         super.__init__(self)
+#         self.setWindowTitle("Rehberim")
 
-        central_widget() == QWidget(self)
-        layout = QVBoxLayout(central_widget)
-        yerlesim2 = QHBoxLayout()
+#         central_widget() == QWidget(self)
+#         layout = QVBoxLayout(central_widget)
+#         yerlesim2 = QHBoxLayout()
 
-        yerlesim2.addWidget(QLabel("ADI :"))
-        Adi=QLineEdit("Ad Girin")
-        yerlesim2.addWidget(Adi)
+#         yerlesim2.addWidget(QLabel("ADI :"))
+#         Adi=QLineEdit("Ad Girin")
+#         yerlesim2.addWidget(Adi)
 
-        yerlesim2.addWidget(QLabel("Telefon :"))
-        tel =QLineEdit("Telefon Girin")
-        yerlesim2.addWidget(tel)
+#         yerlesim2.addWidget(QLabel("Telefon :"))
+#         tel =QLineEdit("Telefon Girin")
+#         yerlesim2.addWidget(tel)
 
-        layout.addLayout(yerlesim2)
+#         layout.addLayout(yerlesim2)
 
-        label_baslik=QLabel("Rehber Başlığı")
-        layout.addWidget(label_baslik)
+#         label_baslik=QLabel("Rehber Başlığı")
+#         layout.addWidget(label_baslik)
 
-        self.setLayout(layout)
-        self.arayuz()
+#         self.setLayout(layout)
+#         self.arayuz()
 
 
 def main():
     app = QApplication(sys.argv)
     window = AnaPencere()
+    window = LoginPencere()
+    window =KayitEklemeEkrani()
     window.show()
     sys.exit(app.exec())
 
