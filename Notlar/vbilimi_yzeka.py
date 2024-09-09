ddeffde46b9abd24d64aa274ed0d052af0955429
beyzaunsal <unsalbeyza678@gmail.com>
2024-09-09 19:26:09 +0300

    güncel

--- Notlar/vbilimi_yzeka.py
+++ Notlar/vbilimi_yzeka.py
@@ -75,14 +75,16 @@
 # print("Tersi:\n",tersi) 
 
 # numpy satırlar ve sütünlar---------------------------
-import numpy as np
+# import numpy as np
 
-arr1 = np.arange(1,21).reshape(5,4)
-print (arr1)
+# arr1 = np.arange(1,21).reshape(5,4)
+# print (arr1)
 
 # print("\nBirinci satır => arr1[0]   : ",arr1[0])
 # print("\nİkinci satır  => arr1[1]   : ",arr1[1])
 # print("\nİlk iki satır => arr1[0:2] : \n",arr1[0:2])
 # print("\nBirinci sütun => arr1[:,0] : ",arr1[:,0])
 # print("\nİkinci sütun  => arr1[:,1] : ",arr1[:,1]) 
-print("\nEleman        => arr1[2,1] : ",arr1[2,1])  
+# print("\nEleman        => arr1[2,1] : ",arr1[2,1])  
+
+
